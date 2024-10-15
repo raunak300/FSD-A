c22ff9a147833c6970380aabfe23b98fe6d9e32e
raunak300 <raunaktomar30@gmail.com>
2024-10-15 15:40:19 +0530

    this is today's work

---
+++ Day-5/script.js
@@ -0,0 +1,30 @@
+// let x=confirm("are you sure to log out");
+// alert(x);
+// let v=prompt("enter name:","rahul");
+// alert(v);
+// let disp=()=>{
+//     console.log("who is this:")
+// }
+// disp();
+// let says=()=>console.log("i am who??");
+// says();
+// let sum=(x,y)=>console.log("is your sum is:",x+y);
+// sum(10,2);
+// display=()=>{
+//     let name=document.querySelectorAll("p");
+//     name[0].innerHTML="palak check here";
+//     name[0].style.backgroundColor="red";
+//     name[1].innerHTML="palak now look there"
+//     name[1].style.backgroundColor="blue";
+// }
+// display=()=>{
+//     let name=document.querySelector("a");
+//     let head=document.createElement("h1");
+//     head.innerText="this is heading";
+//     name.setAttribute('href','https://aktu.ac.in');
+//     name.innerText="AKTU WEBSITE";
+//     let heading=document.querySelector(".heading");
+//     heading.appendChild(head);
+// }
+let btn=document.querySelector("button");
+btn.addEventListener("click",()=>alert("hello"));
